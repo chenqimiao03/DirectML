c2207d6c073d77bcb7882b8d9668aad61ed660c7
陈其淼 <16174103@sdtbu.edu.cn>
2024-05-05 14:18:45 +0800

    first commit

---
+++ models/LeNet.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LeNet_5(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.sigmoid(self.conv1(x))
+        x = self.pool1(x)
+        x = F.sigmoid(self.conv2(x))
+        x = self.pool2(x)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.sigmoid(self.fc1(x))
+        x = F.sigmoid(self.fc2(x))
+        x = self.fc3(x)
+        return x
